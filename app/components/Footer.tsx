4fa77e0f24e7c0bd62a5bbb2bc48799a226910eb
Agawa <t.agawa1128ht@gmail.com>
2023-12-21 10:10:50 +0900

    Footerのロゴをリンク化

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <div className="flex flex-col items-center bg-sub-color py-[50px] text-white">
     <div className="mb-12">
-      <Image 
-        src="/images/icon.png"
-        alt=""
-        width={100}
-        height={100}
-      />
+      <Link href="/">
+        <Image 
+          src="/images/icon.png"
+          alt=""
+          width={100}
+          height={100}
+        />
+      </Link>
     </div>
     <p className="mb-12">
           <span className="mr-2"><small>CREATED&nbsp;BY&nbsp;</small></span>
