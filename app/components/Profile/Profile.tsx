6c0d957442d0d49df8c2c4cf482e975492a175e2
AgawaToshiki <t.agawa1128ht@gmail.com>
2024-12-31 15:40:42 +0900

    文言を変更

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -19,7 +19,7 @@ const Profile = () => {
           <p>
             広島県在住の29歳。<br />
             昔から考えて答えを導くことが好きで、プログラミングというワードに惹かれHTML/CSSの学習を始める。<br />
-            現在はWebエンジニアを目指してフロントエンドはNext.js・React・Vue、バックエンドはNestJS・Expressと幅広く学習を進めています。
+            現在はフロントエンドはNext.js・React・Vue、バックエンドはNestJS・Expressと幅広く学習を進めています。
           </p>
         </div>
       </div>
